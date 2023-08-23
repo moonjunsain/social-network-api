af130c1617cedb14aab22b847933b68e12f6a63b
moonjunsain <moonjunsain@gmail.com>
2023-08-22 17:13:37 -0700

    set up the server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const db = require('./config/connection')
+
 const app = express()
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 const PORT = 3001
+
+db.once('open', ()=>{
+    app.listen(PORT, ()=>{
+        console.log(`App listening on http://localhost:${PORT}`)
+    })
+})
