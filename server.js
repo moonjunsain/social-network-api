6c288aedd015fda9d13e9c462074a040bcfed78a
moonjunsain <moonjunsain@gmail.com>
2023-08-22 17:18:12 -0700

    set up the router

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json())
 
 const PORT = 3001
 
+// setting up routes
+app.use(require('./routes/index'))
+
+// running a server once db is configured
 db.once('open', ()=>{
     app.listen(PORT, ()=>{
         console.log(`App listening on http://localhost:${PORT}`)
